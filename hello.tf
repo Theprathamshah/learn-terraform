d39b4071ded6eccc2a5a97b7f0fb2193e5813cc1
Pratham Shah <itspratham2911@gmail.com>
2025-09-14 15:13:00 +0530

    feat: :sparkles: learned about tf variable and user input

--- hello.tf
+++ hello.tf
@@ -1,7 +1,14 @@
+variable username {
+
+}
 output hello1 {
     value = "Hello World"
 }
 
 output hello2 {
     value = "Hello from Pratham Shah!!"
+}
+
+output hellowithname {
+    value = "Hello, ${var.username}"
 }
